88058d4ae0a9211c41f4b098d72e344f086cb72f
nstonic <nstonic@gmail.com>
2023-09-15 11:32:39 +0400

    Update converter.py

--- backend/src/routes/converter.py
+++ backend/src/routes/converter.py
@@ -9,12 +9,8 @@ currency_router = APIRouter(tags=["converter"])
 @currency_router.get('/rates/')
 async def retrieve_all_events(to: str, value: int, from_: str = Query(alias='from')) -> dict:
     try:
-        data = {
-            "result": Converter().convert(from_, to, value)
-        }
-    except ValueError as ex:
-        return {'error': str(ex)}
-    except (HTTPError, KeyError) as ex:
+        result = Converter().convert(from_, to, value)
+    except (ValueError, HTTPError, KeyError) as ex:
         return {'error': str(ex)}
 
-    return data
+    return {'result': result}
