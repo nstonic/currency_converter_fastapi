274f482ec1fb14a80255289bda945a82690c663a
nstonic <nstonic@gmail.com>
2023-09-15 11:10:25 +0400

    Update converter.py

--- backend/src/services/converter.py
+++ backend/src/services/converter.py
@@ -9,7 +9,7 @@ class Converter:
     def get_rates(self) -> dict:
         response = requests.get(self.currency_rate_source)
         response.raise_for_status()
-        return response.json().get('rates') | {'RUB': 1}
+        return response.json()['rates'] | {'RUB': 1}
 
     def convert(self, from_: str, to: str, value: int) -> float:
         from_ = from_.upper()
